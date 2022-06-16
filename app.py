19c128ea4b48e3718df64b820884bb92e87e110b
Nixuge <mail@nixuge.me>
2022-06-16 13:51:44 +0000

    File cleaner & proper logging

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #!/usr/local/bin/python3
 from gevent.pywsgi import WSGIServer
-from server import APP
-
+from server import APP, Cleaner
 
 if __name__ == "__main__":
+    Cleaner.runCleanerThread()
     http_server = WSGIServer(('', 60218), APP)
     http_server.serve_forever()
--- server.py
+++ server.py
@@ -2,15 +2,30 @@
 
 from dataclasses import dataclass
 import threading
+from typing import Optional
 from flask import Flask, request, send_file, render_template, redirect
 import yt_dlp as ytdl
 import traceback
 import time
 import os
+import logging
 
 APP = Flask(__name__)
 CAV = f"v2.1" #Current API version
 CURRENT_SCRIPT_VERSION = "2.1"
+
+# LOGGING (for some reason logging.getLogger(__name__) doesn't work...)
+logging.addLevelName(logging.DEBUG, "\033[1;36m%s\033[1;0m" % logging.getLevelName(logging.DEBUG)) #37 = gray but blue good
+logging.addLevelName(logging.INFO, "\033[1;36m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+
+logger_format = "[%(levelname)s|%(filename)s:%(lineno)s@%(funcName)s] %(message)s"
+logging.basicConfig(format=logger_format, level=logging.DEBUG)
+
+#avoid yt-dlp output
+ytdl_logger = logging.getLogger("ytdl-ignore")
+ytdl_logger.disabled = True
 
 class Utils:
     @staticmethod
@@ -55,16 +70,16 @@ class Global:
         return CURRENT_SCRIPT_VERSION
 
 
-
 @dataclass
 class Video:
     name: str #name WITHOUT EXTENSION
     expiration_date: int
 
-    def __init__(self, name: str, expiration_date: int or None) -> None:
+    def __init__(self, name: str, expiration_date: Optional[int] = None) -> None:
         if expiration_date == None:
             # expiration_date = (time.time_ns() + 120000000000) #set expiration date 120s in the future
-            expiration_date = (time.time_ns() + 20000000000) #set expiration date 20s in the future
+            expiration_date = (time.time_ns() + 60000000000) #set expiration date 60s in the future
+            # expiration_date = (time.time_ns() + 20000000000) #set expiration date 20s in the future
         
         if type(name) != str:
             name = str(name)
@@ -79,66 +94,64 @@ class Cleaner:
     videos_path = "videos"
     _toClean: list[Video] = []
     _thread: threading.Thread = None
-    _threadStop: bool = False
 
     @staticmethod
     def addVideo(video: Video) -> None:
         if type(video) != Video: return
         Cleaner._toClean.append(video)
+        logging.debug(f"Added video to clean: {video}")
 
     @staticmethod
     def runCleanerThread() -> None:
-        if Cleaner._thread != None: return #Already running
+        if Cleaner._thread != None: 
+            logging.warning("Cleaner already running")
+            return
+        
         Cleaner._thread = threading.Thread(target=Cleaner.threadFunc)
         Cleaner._thread.start()
-
-
-    @staticmethod
-    def stopCleanerThread() -> None:
-        if Cleaner._thread == None: return #Already stopped
-        Cleaner._threadStop = True
-        Cleaner._thread = None #don't think it's needed but clean up memory
+        
 
     @staticmethod
     def threadFunc() -> None: #could make it as a diff class but honestly no need
-        print("Cleaner thread started !")
+        logging.info("Cleaner thread started")
         while True:
             current_time = time.time_ns()
 
             for video in Cleaner._toClean:
                 if video.expiration_date < current_time: #detection & clear from the list
-                    print(f"Video expired: {video.name}")
                     Cleaner._toClean.remove(video)
 
+                    deleted_files: int = 0
                     for file in os.listdir(f"{Cleaner.videos_path}/"): #clear the actual video file
                         if video.name in file:
-                            print(f"Deleted video: {file}")
+                            deleted_files += 1
                             os.remove(f"{Cleaner.videos_path}/{file}")
                     
+                    logging.debug(f"Video expired: {video.name} | Removed {deleted_files} files")
+
             time.sleep(1) #ok stopping the thread since it's running along the main one
-
-
 
 
 class Youtube:
     @APP.route(f"/api/{CAV}/get_best_qualities")
     def get_best_qualities():
+        logging.debug("Getting best qualities")
         headers_data = request.headers
-
         # FORMAT_TYPE = str(headers_data.get("format_type"))
         # could implement, but this is mostly for ios tbh
         URL = headers_data.get("url")
         if not URL:
+            logging.error("Needs an URL")
             return "Please add an URL yo your request."
 
 
-        with ytdl.YoutubeDL({}) as ydl:
+        with ytdl.YoutubeDL({"quiet": True, "logger": ytdl_logger}) as ydl:
             meta = ydl.extract_info(URL, download=False)
             formats = meta.get('formats', [meta])
         
         #get info about the media
         HAS_AUDIO_FORMAT, HAS_VIDEO_FORMAT, IS_GENERIC = Utils.getFormats(formats)
-
+        logging.debug(f"Has audio: {HAS_AUDIO_FORMAT} | Has video: {HAS_VIDEO_FORMAT}")
 
         #generate the final dict
         final_formats = {
@@ -167,7 +180,7 @@ class Youtube:
             "value": None,
             "other": True
         }
-
+        logging.debug(f"Total formats: {len(final_formats)}")
         #make a readable dictionary
         return final_formats
 
@@ -177,9 +190,10 @@ class Youtube:
         headers_data = request.headers
         URL = headers_data.get("url")
         if not URL:
+            logging.error("Needs an URL")
             return "Please add an URL yo your request."
         
-        with ytdl.YoutubeDL({}) as ydl:
+        with ytdl.YoutubeDL({"quiet": True, "logger": ytdl_logger}) as ydl:
             meta = ydl.extract_info(URL, download=False)
             formats = meta.get('formats', [meta])
         
@@ -194,7 +208,7 @@ class Youtube:
         final_formats["Choose custom value"] = {
             "value": "CUSTOM"
         }
-        
+        logging.debug(f"Total formats: {len(final_formats)}")
         return final_formats
 
     @staticmethod
@@ -203,18 +217,24 @@ class Youtube:
         Cleaner.addVideo(Video(CURRENT_TIME))
 
         ydl_opts = {
-            'outtmpl': f"videos/{CURRENT_TIME}.%(ext)s", #.mov needed or else apple doesn't recognize it as a video (thanks apple)
-            'merge_output_format': merge_output_format,
-            'ffmpeg_location': '/usr/bin/ffmpeg',
-            "quiet": True
+            "outtmpl": f"videos/{CURRENT_TIME}.%(ext)s", #.mov needed or else apple doesn't recognize it as a video (thanks apple)
+            "merge_output_format": merge_output_format,
+            "ffmpeg_location": "/usr/bin/ffmpeg",
+            "quiet": True,
+            "logger": ytdl_logger
         }
         if format_id:
             ydl_opts["format"] = format_id
+
+        logging.debug(f"Downloading video {url}")
+        logging.debug(f"Output: {merge_output_format}, Force to: {force_to_format}, ID: {format_id}")
 
         #handle if audio format
         with ytdl.YoutubeDL(ydl_opts) as ydl:
             meta = ydl.extract_info(url)
             formats = meta.get('formats', [meta])
+
+        logging.debug(f"Done downloading {url}")
 
             #get the filemane in a dirty way since yt-dlp doesn't let us get it easily
         FILE_NAME: str
@@ -222,6 +242,8 @@ class Youtube:
             if str(CURRENT_TIME) in file:
                 FILE_NAME = file
 
+        logging.debug(f"Final filename: {FILE_NAME}")
+        
             
         #if audio return as m4a, else return as mov
         for f in formats:
@@ -243,21 +265,25 @@ class Youtube:
         format_id = data.get("id")
 
         url = data.get("url")
-        if not url: return "Please add an url or format id to your request headers."
+        if not url: 
+            logging.error("Needs an URL")
+            return "Please add an url to your request headers."
         
-        format_extension = data.get("format_extension ")
+        format_extension = data.get("format_extension")
         if not format_extension: format_extension = "mov"
 
         #setup that way so that by default it retries as a mkv (which supports pretty much everything)
         dont_retry_as_mkv = data.get("dont_retry_as_mkv")
         
+
         try:
             return Youtube._get_video(format_extension , format_id, url)
         except Exception as e:
+            logging.warning(f"Failed as a {format_extension} ({e})")
             if dont_retry_as_mkv:
                 return str(e)
             else:
-                print("TRYING AS MKV")
+                logging.debug("Retrying as mkv")
                 return Youtube._get_video("mkv", format_id, url)
 
 
