f0cd8596c701c7243628db610d7e96d295b5ad9c
Nixuge <mail@nixuge.me>
2022-10-29 20:20:15 +0200

    Supported sites done, restructuration, fixes

---
+++ static/js/supported_websites.js
@@ -0,0 +1,29 @@
+const website_wrapper = document.getElementById("website_wrapper");
+class Website {
+    constructor(name, info, color_rgba) {
+        this.name = name;
+        this.info = info;
+        this.color = color_rgba;
+        this.dark_color = color_rgba.replace("1)", "0.5)"); //dirty but simple rgba trick
+    }
+}
+
+const websites = [
+    new Website("Youtube", "No playlist, working fine.", "rgba(255, 0, 0, 1)"),
+    new Website("Youtube Music", "Same as Youtube.", "rgba(255, 0, 0, 1)"),
+    new Website("Reddit", "Working fine.", "rgba(255, 67, 0, 1)"),
+    new Website("Twitter", "First media only when multiple in a tweet.", "rgba(29, 161, 242, 1)"),
+    new Website("Instagram", "First media only when multiple available.", "rgba(221, 42, 123, 1)"),
+]
+
+websites.forEach(website => 
+    website_wrapper.innerHTML += generate_website_div(website)
+);
+
+function generate_website_div(website) {
+    return `<div class="website_div biggerfont" style="border: 3px solid ${website.color};">
+        <span class="name" style="border-right: 3px solid ${website.color}; 
+            background: ${website.dark_color}">${website.name}</span>
+        <span class="info">${website.info}</span>
+    </div>`
+}
