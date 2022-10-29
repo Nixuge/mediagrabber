5302818c088a69bc346127d709e2bea2d0abd5de
Nixuge <mail@nixuge.me>
2022-10-29 16:37:43 +0200

    Cache fix again (removed html caching)

--- server.py
+++ server.py
@@ -86,7 +86,7 @@ class Global:
         # otherwise chrome doesn't like it.
         # max-age = 1 day (60*60*24)
         ct = response.content_type
-        if "javascript" in ct or "css" in ct or "html" in ct:
+        if "javascript" in ct or "css" in ct:
             response.headers['Cache-Control'] = 'public, max-age=86400'
 
         return response
