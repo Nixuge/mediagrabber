b5caec447f0fd8aeef438c8046fb8f3b62040c8f
Nixuge <mail@nixuge.me>
2022-10-30 13:30:38 +0100

    Temporarely disabled cache

--- server.py
+++ server.py
@@ -95,8 +95,8 @@ class Global:
         # otherwise chrome doesn't like it.
         # max-age = 1 day (60*60*24)
         ct = response.content_type
-        if "javascript" in ct or "css" in ct:
-            response.headers['Cache-Control'] = 'public, max-age=86400'
+        # if "javascript" in ct or "css" in ct:
+            # response.headers['Cache-Control'] = 'public, max-age=86400'
 
         return response
 
