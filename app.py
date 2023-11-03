d83ddb03f3dfd54779472d50a9b467d81ed0dd24
Nixuge <mail@nixuge.me>
2023-11-03 21:45:35 +0100

    Added yt-dlp version info on startup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 from gevent.pywsgi import WSGIServer
+from yt_dlp import version
 import setup
 
 from cleaning.cleaner import Cleaner
@@ -7,5 +8,6 @@ from utils.variables import Global
 
 if __name__ == "__main__":
     Cleaner.runCleanerThread()
+    print(f"Running yt-dlp version {version.__version__} (channel {version.CHANNEL}, head {version.RELEASE_GIT_HEAD})")
     http_server = WSGIServer(('', 60218), Global.app)
     http_server.serve_forever()
