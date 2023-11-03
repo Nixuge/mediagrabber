72e990ac978e10633bee757dc3ba4dda1a32ae87
Nixuge <mail@nixuge.me>
2023-11-03 21:47:02 +0100

    logging instead of print for last commit

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 #!/bin/python3
+import logging
 from gevent.pywsgi import WSGIServer
 from yt_dlp import version
-import setup
+import setup #important, sets up everything when imported
 
 from cleaning.cleaner import Cleaner
 from utils.variables import Global
 
 if __name__ == "__main__":
     Cleaner.runCleanerThread()
-    print(f"Running yt-dlp version {version.__version__} (channel {version.CHANNEL}, head {version.RELEASE_GIT_HEAD})")
+    logging.info(f"Running yt-dlp version {version.__version__} (channel {version.CHANNEL}, head {version.RELEASE_GIT_HEAD})")
     http_server = WSGIServer(('', 60218), Global.app)
     http_server.serve_forever()
